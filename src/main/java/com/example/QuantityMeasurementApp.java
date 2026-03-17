b9548cfc7f6af3b6189af109514e4bd76c525c32
rachit069 <rachit694.be22@chitkara.edu.in>
2026-03-17 10:24:51 +0530

    UC-2

--- src/main/java/com/example/QuantityMeasurementApp.java
+++ src/main/java/com/example/QuantityMeasurementApp.java
@@ -33,15 +33,54 @@ public class QuantityMeasurementApp {
             return Double.compare(this.value, other.value) == 0;
         }
     }
+    public static class Inch{
+        private final double value;
+
+        public Inch(double value){
+            this.value = value;
+        }
+
+        public double getValue(){
+            return value;
+        }
+
+        public boolean equals(Object obj){
+            if(this == obj){
+                return true;
+            }
+            if(obj == null){
+                return false;
+            }
+            if(obj.getClass() != this.getClass()){
+                return false;
+            }
+
+            Inch other = (Inch) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInch(double v1, double v2) {
+        Inch i1 = new Inch(v1);
+        Inch i2 = new Inch(v2);
+        return i1.equals(i2);
+    }
 
     public static void main(String[] args) {
 
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
-
-        boolean result = feet1.equals(feet2);
-
+        boolean feetResult = compareFeet(1.0, 1.0);
         System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + result + ")");
+        System.out.println("Output: Equal (" + feetResult + ")");
+
+        boolean inchResult = compareInch(2.0, 1.0);
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + inchResult + ")");
     }
 }
--- src/test/java/com/example/QuantityMeasurementAppTest.java
+++ src/test/java/com/example/QuantityMeasurementAppTest.java
@@ -41,4 +41,20 @@ public class QuantityMeasurementAppTest {
 
         assertFalse(feet1.equals(obj), "Feet should not equal non-numeric object");
     }
+
+    @Test
+    void testInchEquality_SameValue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    void testInchEquality_DifferentValue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(2.0);
+
+        assertFalse(i1.equals(i2));
+    }
 }
